d5749a7d2305038661f52cb3312a9fce4277e342
federalolive <j3pumphrey@gmail.com>
2021-02-08 11:33:33 -0600

    Create part 1 of model to test functionality.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+import random
 
-# Create your models here.
+class Passwd(models.Model):
+    name = models.CharField(max_length=100)
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def passwds_index(request):
+    return render(request, 'passwds/index.html')
