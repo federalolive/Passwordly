78103b8447295a9fc5a6e758bf3cbfcb84dfec14
federalolive <j3pumphrey@gmail.com>
2021-02-11 12:50:36 -0600

    Add user to models.

--- main_app/models.py
+++ main_app/models.py
@@ -8,6 +8,7 @@ class Vault(models.Model):
     site = models.CharField(max_length=200)
     description = models.TextField(max_length=200)
     pw = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
