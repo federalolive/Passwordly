4187bed1b4b1099aa1594c288d595ea07932f428
federalolive <j3pumphrey@gmail.com>
2021-02-11 12:38:44 -0600

    Rename model from Store to Vault for ease of use

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-import random
 
-class Passwd(models.Model):
-    name = models.CharField(max_length=100)
-    
+
+class Vault(models.Model):
+    name = models.CharField(max_length=75)
+    site = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
+    pw = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pw_id': self.id})
+
+        
