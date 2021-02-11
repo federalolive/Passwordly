98d717c42510316b6c28fa0c73ca988fa3930506
federalolive <j3pumphrey@gmail.com>
2021-02-11 10:21:25 -0600

    Add url pattern for accounts.

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('passwds/', views.passwds_index, name='index'),
+    path('stores/', views.stores_index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('password/', views.generate_password, name='password'),
 ]
