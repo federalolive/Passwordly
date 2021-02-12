583a866cda00aa29f0659575760d9af716723e7a
federalolive <j3pumphrey@gmail.com>
2021-02-12 12:14:26 -0600

    Create VaultForm.

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.forms import ModelForm
+from .models import Vault
 class PasswordGeneratorForm(forms.Form):
   length = forms.IntegerField(min_value=4, max_value=128)
   use_upper = forms.BooleanField(initial=True, required=False)
@@ -7,3 +8,9 @@ class PasswordGeneratorForm(forms.Form):
   use_digits = forms.BooleanField(initial=True, required=False)
   use_special = forms.BooleanField(initial=True, required=False)
   avoid_similar = forms.BooleanField(initial=True, required=False)
+
+class VaultForm(forms.ModelForm):
+  
+    class Meta:
+      model = Vault
+      fields = ['name', 'site', 'description', 'pw']
