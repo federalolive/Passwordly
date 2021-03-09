6732b448f22ff4b3ee43285850d64748a759feba
federalolive <j3pumphrey@gmail.com>
2021-03-08 18:40:02 -0600

    Add update urls.

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('vaults/<int:vault_id>/', views.vaults_detail, name='detail'),
     path('vaults/<int:pk>/delete/', views.VaultDelete.as_view(), name='vaults_delete'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('vaults/<int:pk>/update/', views.VaultUpdate.as_view(), name='vaults_update'),
     path('accounts/signup/', views.signup, name='signup'),
     path('password/', views.generate_password, name='password'),
     path('passwords/add_pw/', views.add_passwordgenerator, name='add_gen_pass'),
